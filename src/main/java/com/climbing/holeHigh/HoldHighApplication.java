371ce04c7b2024913fb3583840732a5ddb7694af
Reese <ajw5921@gmail.com>
2024-11-24 16:45:42 +0900

    project init

---
+++ src/main/java/com/climbing/holeHigh/HoldHighApplication.java
@@ -0,0 +1,13 @@
+package com.climbing.holeHigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldHighApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climbing/holeHigh/HoldHighApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climbing.holeHigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldHighApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
